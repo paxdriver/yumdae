b78fa72d4614d4f03dcdecfbce7e05dee3d5b32d
Kristopher Driver <paxdriver@gmail.com>
2020-10-23 17:37:23 -0500

    Updates

---
+++ precache-manifest.0c1402c7d73f12a3897bdb777a5a4e4f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13861fca019130b51f153317d06bd968",
+    "url": "/paxdriver/yumdae.git/index.html"
+  },
+  {
+    "revision": "a8e0a35bac358c1ccf67",
+    "url": "/paxdriver/yumdae.git/static/css/main.0288b20f.chunk.css"
+  },
+  {
+    "revision": "e292d9b74bd7d54bab5b",
+    "url": "/paxdriver/yumdae.git/static/js/2.589da2f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/paxdriver/yumdae.git/static/js/2.589da2f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8e0a35bac358c1ccf67",
+    "url": "/paxdriver/yumdae.git/static/js/main.57b2251b.chunk.js"
+  },
+  {
+    "revision": "f6b1e00f9aa850c0ea0d",
+    "url": "/paxdriver/yumdae.git/static/js/runtime-main.03957208.js"
+  },
+  {
+    "revision": "ac8153183d05d641d432ab5e5deb7338",
+    "url": "/paxdriver/yumdae.git/static/media/yumdae.ac815318.png"
+  },
+  {
+    "revision": "073e85ef011d5df76cb2048cb2182f4b",
+    "url": "/paxdriver/yumdae.git/static/media/yumdaeB.073e85ef.png"
+  }
+]);
